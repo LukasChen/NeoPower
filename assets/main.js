4e1e732923e1c600927401d828235737e0e68b37
LukasChen <villagelukaschen@gmail.com>
2019-04-18 21:53:58 +0800

    Semi working filter animation

--- assets/main.js
+++ assets/main.js
@@ -52,110 +52,86 @@ $(function() {
   });
 
   var filterItems = $(".filter-items > *");
-  var filterTl = new TimelineLite();
+  //var filterTl = new TimelineLite();
 
   if (window.location.hash) {
     console.log(window.location.hash);
-    filterItem("." + window.location.hash.substr(1));
+    filterItem(filterItems,$("." + window.location.hash.substr(1)));
     $("[data-toggle='filter'][href='" + "." + window.location.hash.substr(1) + "']").addClass("active");
   }
 
   $("[data-toggle='filter']").on("click",function(event) {
     event.preventDefault();
 
-    filterTl = new TimelineLite();
+    //filterTl = new TimelineLite();
 
     $(this).closest(".filter-sidebar").find(".nav-link").removeClass("active");
-
     $(this).addClass("active");
     
     var target = $(this).attr("href");
     window.location.hash = target.substr(1);
 
-    filterItem(target);
-
-    // filterItems.each(function() {
-    //   var $this = $(this);
-    //   TweenLite.to($(this),0.3,{opacity: 0, ease: Power1.easeInOut, onComplete: function() {
-    //     $this.hide();
-    //   }});
-    // });
-
-    
+    filterItem(filterItems,$(target));
   });
 
-  function filterItem(target) {
-    filterTl.to(filterItems,0.3,{opacity: 0, ease: Power1.easeInOut});
-    filterTl.to(filterItems,0,{display: "none"});
-    filterTl.to($(target),0,{display: "block"});
-    filterTl.to($(target),0.3,{opacity: 1, ease: Power1.easeInOut});
+  function filterItem(filterItems,target) {
+
+    var animationName = 'bounce';
+    //target.off("animationend");
+    //filterItems.off("animationend");
+    console.log("filteritems" + new Date());
+
+    filterItems.addClass(animationName +  "Out fast animated")
+    filterItems.one('animationend',function () {
+      filterItems.hide();
+      filterItems.removeClass(animationName +  "Out fast animated");
+      console.log("animationEnd");
+
+      target.show();
+      target.addClass(animationName +  "In fast animated");
+
+      target.one('animationend',function() {
+        target.removeClass(animationName +  "In fast animated");
+      });
+    });
+    // filterTl.to(filterItems,0.3,{opacity: 0, ease: Power1.easeInOut});
+    // filterTl.to(filterItems,0,{display: "none"});
+    // filterTl.to($(target),0,{display: "block"});
+    // filterTl.to($(target),0.3,{opacity: 1, ease: Power1.easeInOut});
 
     $(".filter-reset").show();
-    TweenLite.to($(".filter-reset"),0.3,{opacity: 1, ease: Power1.easeInOut});
+    $(".filter-reset").animateCss("fadeIn");
   }
 
   $(".filter-reset").on("click",function() {
     event.preventDefault();
     history.pushState("", document.title, window.location.pathname+ window.location.search);
     $(this).closest(".filter-sidebar").find(".nav-link").removeClass("active");
+    filterItems.off("animationend");
     filterItems.show();
-    TweenLite.to(filterItems,0.3,{opacity: 1, ease: Power1.easeInOut});
-    var $this = $(this);
-    TweenLite.to($(this),0.3,{opacity: 0, ease: Power1.easeInOut, onComplete: function () {
-      $this.hide();
-    }});
+    filterItems.addClass('bounceIn fast animated');
+    filterItems.one('animationend',function () {
+      filterItems.removeClass("bounceIn fast animated");
+    });
+    $(this).animateCss("fadeOutLeft",function() { $(".filter-reset").hide() });
+    console.log('fadeOut');
   });
 
+});
 
-
-  // var tl = new TimelineLite();
-  // var tl2 = new TimelineLite();
-  // var controller = new ScrollMagic.Controller();
-
-  // var liftTween = TweenLite.from("#lift-img",2,{x: -200,opacity: 0,ease: Power1.easeInOut});
-  /*
-  $(".product-overview-style-1 .product-overview-img-1").each(function(index,el) {
-    tl.from(el,1.5,{y: 300, ease: Power1.easeInOut},'-=1');
-  });
-  */
-  /*
-  $(".product-overview-style-2 .product-overview-img-2").each(function(index,el) {
-    tl2.from(el,1.5,{y: 300, ease: Power1.easeInOut},'-=1');
-  });
-  */
-  /*
-  var scene1 = new ScrollMagic.Scene({
-    triggerElement: ".product-overview-style-1",
-    triggerHook:"onEnter"
-  })
-  .setTween(tl)
-  .addTo(controller);
-  */
-  /*
-  var scene2 = new ScrollMagic.Scene({
-    triggerElement: ".product-overview-style-2",
-    triggerHook: "onEnter"
-  })
-  .setTween(tl2)
-  .addTo(controller);
-  */
-
-  function animateCSS(element, animationName, speed, callback) {
-    element.addClass('animated');
-    element.addClass(animationName)
-    if (speed) element.addClass(speed);
-
-    function handleAnimationEnd() {
-        element.removeClass('animated');
-        element.removeClass(animationName);
-        if (speed) element.removeClass(speed);
-        console.log("animate End!");
-        element.off('animationend', handleAnimationEnd);
-
-        if (typeof callback === 'function') callback()
-    }
-
-    element.on('animationend', handleAnimationEnd);
-}
-    
+$.fn.extend({
+  animateCss: function (animationName, callback) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    // if (!Array.isArray(animationNames)) {
+    //   animationNames = [animationNames];
+    // }
+    this.addClass('animated');
+    $(this).addClass(animationName).one(animationEnd, function () {
+      $(this).removeClass(animationName);
+      if (typeof(callback) === 'function') {
+        callback();
+      }
+    });
+    return this;
+  }
 });
